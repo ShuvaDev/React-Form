5fa91ce104bad6a7e09193a52393d46bdab21333
ShuvaDev <s.r.shuvadeb@gmail.com>
2020-05-26 00:01:57 +0600

    Updates

---
+++ precache-manifest.a83c460f9f803f05d25fe6f5b7578940.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eda1a432e7d6461af66e568c308cc7ee",
+    "url": "/React-Form/index.html"
+  },
+  {
+    "revision": "77b151fa22cde460b467",
+    "url": "/React-Form/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "77b151fa22cde460b467",
+    "url": "/React-Form/static/js/2.7880d44f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Form/static/js/2.7880d44f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a4470a5884c3770018a",
+    "url": "/React-Form/static/js/main.7053a2df.chunk.js"
+  },
+  {
+    "revision": "d563e95564e6fe18fbf6",
+    "url": "/React-Form/static/js/runtime-main.148d51b7.js"
+  }
+]);
